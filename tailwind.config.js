6e300ad4fc832a1bd4605246b0819f0ee86486bc
Omar Eltantawy <110872105+omar01140@users.noreply.github.com>
2024-10-21 08:57:20 +0300

    first part

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,25 @@ module.exports = {
   content: ['./*.html',],
   theme: {
     colors: {
-      'main':'#d667a6'
+      'Background':'#2B2B2B',
+      'Background-Secondry':'#3B3B3B',
+      'Label':'#858584',
+      'Text':'#FFFFFF',
+      'Action':'#A259FF',
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        'work-sans': ['"Work Sans"', 'Helvetica'],
+        'space-mono': ['"Space Mono"', 'monospace'],
+      },
+      fontSize: {
+        'h4': ['28px', { lineHeight: '1.4', fontWeight: 600}],
+        'h5': ['20px', {lineHeight: '1.4', fontWeight: 600}],
+        'Base(body)': ['16px', { lineHeight: '1.4' , fontWeight: 300}],
+        'h5-spaceMono': ['22px', {lineHeight: '1.6', fontWeight: 700}],
+        'h-spaceMono': ['16px', {lineHeight: '1.4', fontWeight: 700}],
+      },
+    },
   },
   plugins: [],
 }
